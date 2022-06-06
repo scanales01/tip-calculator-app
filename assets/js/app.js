915f7f33e49ee4a5493fbeaa6f09cd6b8ecc8a71
scanales01 <scanal01@outlook.com>
2022-06-05 22:12:01 -0500

    update to functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,67 @@
-// class Calculator {
-//     constructor(tipTextElement, totalTextElement) {
-//         this.tipTextElement = tipTextElement
-//         this.totalTextElement = totalTextElement
-//         this.reset()
-//     }
-
-//     reset() {
-//         this.tip = '0.00'
-//         this.total = '0.00'
-//         this.bill = '0.00'
-//         this.people = '0.00'
-//         this.tipSelection = undefined
-//     }
-
-//     update() {
-//         if (this.tipSelection != null) {
-//         } else {
-//             this.tipTextElement.innerText = '0.00'
-//             this.totalTextElement.innerText = '0.00'
-//         }
-//     }
-// }
-
-// const tipButtons = document.querySelectorAll('[data-number]')
-// const inputs = document.querySelectorAll('[data-input]')
-// const resetButton = document.querySelector('[data-reset]')
-// const tipTextElement = document.querySelector('[data-tip]')
-// const totalTextElement = document.querySelector('[data-total]')
-
-// const calculator = new Calculator(tipTextElement, totalTextElement)
-
-// resetButton.addEventListener('click', button => {
-//     calculator.clear()
-//     calculator.updateDisplay()
-//   })
+let bill = undefined;
+let people = undefined;
+let tip = undefined;
 
 // reset calculator
 function reset() {
     document.getElementById("bill").outerHTML = '<input type="text" id="bill"  name="bill" value=0.00 required>'
+    document.getElementById("custom").outerHTML = '<button id="custom" onclick="setInputField()">';
+    document.getElementById("custom").innerHTML = 'Custom';
     document.getElementById("people").outerHTML = '<input type="number" id="people" name="people"  value="0" min="1" step="1" required>'
     document.getElementById("tip").innerText = '0.00'
     document.getElementById("total").innerText = '0.00'
 }
 
-function calculate(tipPercent) {
-    let bill = Number(document.getElementById("bill").value)
-    let people = Number(document.getElementById("people").value)
-
-    if(people < 1) {
-    } else {
-        let tipAmount = (bill * tipPercent)/people
-        let total = (bill + tipAmount)/people
-
-        document.getElementById("tip").innerText = tipAmount
-        document.getElementById("total").innerText = total
-    }
+function formatNumbers(number) {
+    return Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(number);
 }
+
+function checkPeople() {
+    if (people < 1) {
+        document.getElementById("people").style.border = "2.5px solid red";
+    } else {
+        document.getElementById("people").style.border = "transparent";
+        calculate();
+    }
     
+}
+
+// calculate tip and total
+function calculate() {
+    if (tip === undefined) {
+        tip = 0;
+    }
+    let tipAmount = (bill * tip)/people;
+    let total = (bill + tip)/people;
+
+    document.getElementById("tip").innerText = formatNumbers(tipAmount);
+    document.getElementById("total").innerText = formatNumbers(total);
+}
+
+function update() {
+    setBill();
+    setPeople();
+    checkPeople();
+}
+
+function setBill() {
+    bill = Number(document.getElementById("bill").value);
+}
+
+function setPeople() {
+    people = Number(document.getElementById("people").value);
+}
+
+function setTip(newTip) {
+    tip = newTip;
+    update();
+}
+
+function setInputField() {
+    document.getElementById("custom").outerHTML = '<input type="text" id="custom" name="custom" value="0" min="0">';
+    document.getElementById("custom").focus();
+}
 
